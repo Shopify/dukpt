b15331f94bc10e613334a27c9b61a219e39f369a
Ryan Balsdon <ryan.balsdon@shopify.com>
2015-01-13 16:41:51 -0500

    Testing an encrypter

---
+++ lib/dukpt/encrypter.rb
@@ -0,0 +1,29 @@
+module DUKPT
+  class Encrypter
+    include Encryption
+    
+    attr_reader :bdk
+
+    def initialize(bdk, mode=nil)
+      @bdk = bdk
+      self.cipher_mode = mode.nil? ? 'cbc' : mode
+    end
+
+    def encrypt(plaintext, ksn)
+      ipek = derive_IPEK(bdk, ksn)
+      pek = derive_PEK(ipek, ksn)
+      encrypted_cryptogram = triple_des_encrypt(pek, plaintext.unpack("H*").first).upcase
+    end
+
+    def encrypt_pin_block(plaintext, ksn)
+      decrypt(cryptogram, ksn)
+    end
+
+    def encrypt_data_block(plaintext, ksn)
+      ipek = derive_IPEK(bdk, ksn)
+      dek = derive_DEK(ipek, ksn)
+      encrypted_cryptogram = triple_des_encrypt(dek, plaintext.unpack("H*").first).upcase
+    end
+
+  end
+end
